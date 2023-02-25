35bf710fb296d3c742411dbda0bbd15c2cbd2deb
ehasanulh1 <ehasanulh1@gmail.com>
2023-02-25 14:15:54 +0600

    create bacis server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('Hello from node mongo server');

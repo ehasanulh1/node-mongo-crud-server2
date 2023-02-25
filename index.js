2df6bf8779cc478c95c8ecbeb166fcf53e3d0ed6
ehasanulh1 <ehasanulh1@gmail.com>
2023-02-25 14:16:51 +0600

    create mongo ac and paste code in server

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// username: client02
+// password: sUM249tvB1zMxggA
+
+const uri = "mongodb+srv://client02:sUM249tvB1zMxggA@cluster0.m8kuhin.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err));
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello from node mongo server');
